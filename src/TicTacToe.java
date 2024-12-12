6ea5e43c37eeea15d69f46992ec352cf8acbe334
Theo Enfedaque <t.enfedaque@lprs.fr>
2024-12-12 16:31:27 +0100

    feat: ajout historique de victoire et reglage de bug

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe {
         String[] joueur = new String[3];
         boolean gagner = false;
         int ligne, colonne, temponLigne, temponColonne;
+        String victoire = "";
 
         System.out.println("Qui va jouer ?");
         System.out.print("Joueur 1 ton pseudo : ");
@@ -22,8 +23,8 @@ public class TicTacToe {
                     morpion[i][j] = '-';
                 }
             }
-
             while(!gagner){
+                gagner = false;
                 for (int i = 0; i < 2; i++) {
                     for (int j = 0; j < morpion.length; j++) {
                         for (int k = 0; k < morpion[0].length; k++) {
@@ -35,9 +36,9 @@ public class TicTacToe {
                     do{
                         System.out.println(joueur[i]+ " où voulez vous jouer ?");
                         System.out.println("La ligne ? : ");
-                        temponLigne = clavier.nextInt();
+                        temponLigne = nombreInt();
                         System.out.println("La colonne ? : ");
-                        temponColonne = clavier.nextInt();
+                        temponColonne = nombreInt();
                     }while(morpion[temponLigne-1][temponColonne-1] == 'O'||morpion[temponLigne-1][temponColonne-1] == 'X');
                     ligne = temponLigne;
                     colonne = temponColonne;
@@ -56,7 +57,15 @@ public class TicTacToe {
                             (morpion[0][0] == 'X' && morpion[1][1] == 'X' && morpion[2][2] == 'X')||
                             (morpion[0][2] == 'X' && morpion[1][1] == 'X' && morpion[2][0] == 'X')) {
                         gagner = true;
+                        for (int j = 0; j < morpion.length; j++) {
+                            for (int k = 0; k < morpion[0].length; k++) {
+                                System.out.print(morpion[j][k]+" ");;
+                            }
+                            System.out.println();
+                        }
                         System.out.println(joueur[0] + " vous avez gagné !");
+                        victoire += joueur[0]+", ";
+                        i=2;
                     }
                     if ((morpion[0][0] == 'O' && morpion[0][1] == 'O' && morpion[0][2] == 'O')||
                             (morpion[1][0] == 'O' && morpion[1][1] == 'O' && morpion[1][2] == 'O')||
@@ -67,19 +76,38 @@ public class TicTacToe {
                             (morpion[0][0] == 'O' && morpion[1][1] == 'O' && morpion[2][2] == 'O')||
                             (morpion[0][2] == 'O' && morpion[1][1] == 'O' && morpion[2][0] == 'O')) {
                         gagner = true;
+                        for (int j = 0; j < morpion.length; j++) {
+                            for (int k = 0; k < morpion[0].length; k++) {
+                                System.out.print(morpion[j][k]+" ");;
+                            }
+                            System.out.println();
+                        }
                         System.out.println(joueur[1] + " vous avez gagné !");
+                        victoire += joueur[1]+", ";
+                        i=2;
                     }
                 }
             }
 
 
-
-            System.out.println("oui ou non");
+            System.out.println(victoire);
+            System.out.println("voulez vous rejouer ? oui ou non");
             rejouer = clavier.nextLine().toUpperCase();
 
             joueur[2] = joueur[0];
             joueur[0] = joueur[1];
             joueur[1] = joueur[2];
         }
+    }
+
+    public static int nombreInt() {
+        Scanner clavier = new Scanner(System.in);
+        while (!clavier.hasNextInt()) {
+            System.out.println("Pas un nombre");
+            clavier.nextLine();
+            System.out.println("Entrer un nombre SANS virgule");
+        }
+
+        return clavier.nextInt();
     }
 }
